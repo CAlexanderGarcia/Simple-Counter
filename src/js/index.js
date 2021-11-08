fff0a2c02361f9d650e8dd9fbaf21b9d251873c4
CAlexanderGarcia <calexandergarcia@gmail.com>
2021-11-08 11:19:17 +0000

    Trabajo Terminado

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -8,8 +7,19 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import Home from "./component/home.jsx";
-
+import SecondsCounter from "./component/secondsCounter.jsx";
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let timing = 0;
+setInterval(() => {
+	ReactDOM.render(
+		<SecondsCounter
+			digitFour={Math.floor(timing / 1000) % 10}
+			digitThree={Math.floor(timing / 100) % 10}
+			digitTwo={Math.floor(timing / 10) % 10}
+			digitOne={timing % 10}
+		/>,
+		document.querySelector("#app")
+	);
+	timing += 1;
+	console.log(timing);
+}, 1000);
